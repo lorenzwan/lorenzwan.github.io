54d036a59cf8efd74c9b611a7b5d5b0e500553a0
Lorenz Wan <lorenzwch@gmail.com>
2024-03-25 12:46:41 -0400

    Nextra

---
+++ next.config.js
@@ -0,0 +1,17 @@
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+})
+
+module.exports = withNextra({
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+        port: '',
+        pathname: '/lorenzwan/**',
+      },
+    ],
+  },
+})
---
+++ theme.config.tsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+import Image from 'next/image'
+
+const config: DocsThemeConfig = {
+  useNextSeoProps() {
+    return {
+      titleTemplate: 'Lorenz Wan - %s'
+    }
+  },
+  head: (
+    <>
+      <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/lorenzwan/media_src/e4f517f3f39b6ebc8b700060fa035c5df713db88/images/favicon.ico"/>
+    </>
+  ),
+  logo: <>
+    <Image src='https://raw.githubusercontent.com/lorenzwan/media_src/main/images/logoClean.png' width={30} height={30} alt='Lorenz Wan | Rigger'/>
+    <span style={{marginLeft: '.6em', fontWeight: 500}}>
+      Lorenz Wan
+    </span>
+  </>,
+  primaryHue: {
+    dark: 38,
+    light: 32,
+  },
+  project: {
+    link: 'https://github.com/lorenzwan',
+  },
+  chat: {
+    link: 'https://www.linkedin.com/in/lorenzwan',
+    icon: (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+      <path
+        fill="currentColor"
+        d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm-2 16h-2v-6h2v6zm-1-6.891c-.607 0-1.1-.496-1.1-1.109 0-.612.492-1.109 1.1-1.109s1.1.497 1.1 1.109c0 .613-.493 1.109-1.1 1.109zm8 6.891h-1.998v-2.861c0-1.881-2.002-1.722-2.002 0v2.861h-2v-6h2v1.093c.872-1.616 4-1.736 4 1.548v3.359z"
+      />
+      <title>Linkedin</title>
+    </svg>
+    )
+  },
+  docsRepositoryBase: 'https://github.com/lorenzwan/documentation-starter-kit/tree/main',
+  feedback: {
+    content: null,
+  },
+  editLink: {
+    component: null,
+  },
+  toc: {
+    backToTop: true,
+  },
+  gitTimestamp: null,
+  navigation: false,
+  footer: {
+    text: '© 2016-Present Lorenz Wan - All Rights Reserved.',
+  },
+}
+
+export default config
